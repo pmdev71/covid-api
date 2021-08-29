88abe73efa812c2b6a8693188cf9b118f09894ed
Palash Mandal <pmpalashmandal@hmail.com>
2021-08-29 12:08:16 +0600

    first commit

---
+++ js/covid.js
@@ -0,0 +1,49 @@
+const loadCountryName = () =>{
+    fetch('https://api.covid19api.com/countries')
+    .then(res => res.json())
+    .then(data => viewCountry(data));
+}
+
+viewCountry = (data) => {
+    //console.log(data);
+    for(country of data){
+        console.log(country.Country);
+    }
+}
+loadCountryName();
+
+
+
+const Selector = document.getElementById('selector');
+const Btn = document.getElementById('btn');
+Btn.addEventListener('click',function(event){
+            //event.preventDefault();
+            // show the selected index
+            loadCovid(Selector.value);
+});
+
+
+const loadCovid = (countryName = 'bangladesh') => {
+    const api = `https://api.covid19api.com/live/country/${countryName}`;
+    fetch(api)
+    .then(res => res.json())
+    .then(data => displayData(data, data.length));
+};
+
+
+const displayData = (data, length) =>{
+    const updateSection = document.getElementById('update-section');
+    updateSection.textContent = '';
+    console.log(data[length-1]);
+    const dataToday = (data[length-1]);
+    const div = document.createElement('div');
+    div.innerHTML = `<h3> Country  : ${dataToday.Country} </h3>
+    <h4> Active  : ${dataToday.Active} </h4>
+    <h4> Confirmed  : ${dataToday.Confirmed} </h4>
+    <h4> Death  : ${dataToday.Deaths} </h4>
+    <h4> Date  : ${dataToday.Date} </h4>`;
+
+    updateSection.appendChild(div);
+
+  
+};
